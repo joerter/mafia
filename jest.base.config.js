a9dcd7562b80467401168856b2d4dfcf46a64fc9
John Oerter <john.oerter@fcsamerica.com>
2020-04-20 06:59:32 -0500

    switch to jest

---
+++ jest.base.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    preset: 'jest-preset-angular',
+    rootDir: './src/',
+    setupFilesAfterEnv: ['<rootDir>/test.ts'],
+    testURL: 'http://localhost',
+    globals: {
+        'ts-jest': {
+            tsConfig: './tsconfig.spec.json',
+            stringifyContentPathRegex: '\\.html$',
+            astTransformers: [
+                require.resolve(
+                    'jest-preset-angular/InlineHtmlStripStylesTransformer'
+                ),
+            ],
+        },
+    },
+    transform: {
+        '^.+\\.(ts|js|html)$': 'ts-jest',
+    },
+    transformIgnorePatterns: ['node_modules/(?!@ngrx)'],
+    snapshotSerializers: [
+        'jest-preset-angular/AngularSnapshotSerializer.js',
+        'jest-preset-angular/HTMLCommentSerializer.js',
+    ],
+};
